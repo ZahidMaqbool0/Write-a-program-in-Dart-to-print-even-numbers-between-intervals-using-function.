8fd8fe8325e86f02a6cdfa9f3da642a6922af8bb
Zahid Maqbool <167357830+ZahidMaqbool0@users.noreply.github.com>
2025-10-24 20:08:07 +0500

    first commit

---
+++ Write a program in Dart to print even numbers between intervals using function..dart
@@ -0,0 +1,36 @@
+// Write a program in Dart to print even numbers between intervals using function.
+
+import 'dart:io';
+
+main(){
+
+  print('print even numbers between intervals');
+  print('Enter Starting number');
+  int startingNumber = int.tryParse(stdin.readLineSync() ?? '10') ?? 10;
+  print('Enter Ending number');
+  int endingNumber = int.tryParse(stdin.readLineSync() ?? '20') ?? 20;
+
+  findEven(int startingNumber, int endingNumber){
+    if(startingNumber > endingNumber){
+      int tempnumber = startingNumber;
+      startingNumber = endingNumber;
+      endingNumber = tempnumber;
+      for(int i=startingNumber; i<=endingNumber; i++){
+        if(i % 2 == 0){
+          print(i);
+        }
+      }
+    }
+    for(int i=startingNumber; i<=endingNumber; i++){
+      if(i % 2 == 0){
+        print(i);
+      }
+    }
+
+  }
+
+  findEven(startingNumber, endingNumber);
+
+  print('Program End.....!');
+
+}
